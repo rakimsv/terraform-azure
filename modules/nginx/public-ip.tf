9e866b997772390ae62bb554bfbfa2a44b906988
Rakim <rakimsinghverma@gmail.com>
2019-11-28 10:08:07 +0000

    created Thursday branch

--- modules/nginx/public-ip.tf
+++ modules/nginx/public-ip.tf
@@ -3,6 +3,6 @@ resource "azurerm_public_ip" "nginx" {
     location                     = var.resource_group.location
     resource_group_name          = var.resource_group.name
     allocation_method            = "Dynamic"
-    domain_name_label = "${terraform.workspace}-nginx-bob-crutchley"
+    domain_name_label = "${terraform.workspace}-nginx-rakim"
 }
 
